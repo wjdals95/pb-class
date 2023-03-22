66f3cdf224f669a05f91a6851b1d7e1a12cd94a3
wjdals95 <wjdals992@naver.com>
2023-03-22 19:03:29 +0900

    스와이퍼, 텍스트 애니메이션 추가

--- week2-basic/js/reset.js
+++ week2-basic/js/reset.js
@@ -3,13 +3,4 @@ var swiper = new Swiper(".swiper-container", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  direction: 'horizontal',
-  slidesPerView: 1,
-  
-  loop: true,
-  loopAdditionalSlides: 1,
 });

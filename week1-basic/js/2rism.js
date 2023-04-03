1575c689d66c6c93a39d155e43677c00b8345dbf
wjdals95 <wjdals992@naver.com>
2023-04-03 12:11:46 +0900

    dataset js/html 변경

--- week1-basic/js/2rism.js
+++ week1-basic/js/2rism.js
@@ -15,69 +15,19 @@ window.addEventListener("scroll", () => {
   const popularTop = document.querySelector('#popular').getBoundingClientRect().top;
   //해당 섹션의 높이
   const popularHeight = document.querySelector('#popular').getBoundingClientRect().height;
-  console.log(popularHeight)
+  console.log(scrolled)
 
   for(i = 0; i<fadeIn.length; i++){
 
     const fadePercent = fadeIn[i].dataset.fade;
     let fadeTop = fadeIn[i].getBoundingClientRect().top;
 
-    if(fadeTop < (windowHeight - fadeTop / fadePercent)){
+    if(fadeTop < (windowHeight * fadePercent)){
       fadeIn[i].classList.add('active')
+  
     }
     else{
       fadeIn[i].classList.remove('active')
     }
   }
 });
-
-
-
-
-// var scrollEvent = function () {
-//   // 사용자 모니터 화면 높이 + 스크롤이 움직인 높이
-//   var scroll = window.innerHeight + window.scrollY;
-
-//   // 애니메이션 효과를 넣을 DOM 객체 배열
-//   var itemList = document.querySelectorAll("#contents .animatable");
-
-//   Array.prototype.forEach.call(itemList, function (div) {
-//     // 객체 위치와 높이 비교 : 화면에 표출되는 높이인지 체크
-//     if (div.offsetTop < scroll) {
-//       // 객체 animatable 클래스 지우고, animated 클래스 추가
-//       div.classList.remove("animatable");
-//       div.classList.add("animated");
-//     }
-//   });
-// };
-
-// var animateHTML = function () {
-//   var elems, windowHeight;
-
-//   var init = function () {
-//     elems = document.getElementsByClassName("hidden");
-//     windowHeight = window.innerHeight;
-//     _addEventHandlers();
-//   };
-
-//   var _addEventHandlers = function () {
-//     window.addEventListener("scroll", _checkPosition);
-//     window.addEventListener("resize", init);
-//   };
-//   var _checkPosition = function () {
-//     for (var i = 0; i < elems.length; i++) {
-//       var posFromTop = elems[i].getBoundingClientRect().top;
-//       if (posFromTop - windowHeight <= 0) {
-//         elems[i].className = elems[i].className.replace("hidden", "fade-in");
-//       } else {
-//         elems[i].className = elems[i].className.replace("fade-in", "hidden");
-//       }
-//     }
-//   };
-
-//   return {
-//     init: init,
-//   };
-// };
-
-// animateHTML().init();

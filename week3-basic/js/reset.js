3cd2623d7dc76371a52f69b54c7c59b07598f2ac
wjdals95 <wjdals992@naver.com>
2023-04-07 17:45:08 +0900

    헤더 사라지기 transition주기

--- week3-basic/js/reset.js
+++ week3-basic/js/reset.js
@@ -8,13 +8,11 @@ tabs.forEach((tab, index) => {
   tab.addEventListener("mouseover", () => {
     contents[index].classList.add("show");
     tabs[index].classList.add("tabActive");
-    tabContents[index].classList.add("active1");
   });
 
   tab.addEventListener("mouseout", () => {
     contents[index].classList.remove("show");
     tabs[index].classList.remove("tabActive");
-    tabContents[index].classList.remove("active1");
   });
 });
 
@@ -52,14 +50,14 @@ window.addEventListener("keyup", (e) => {
     : "";
 });
 
-const io = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    entry.isIntersecting
-      ? entry.target.classList.add("active")
-      : entry.target.classList.remove("active");
-  });
-});
+// const io = new IntersectionObserver((entries, observer) => {
+//   entries.forEach((entry) => {
+//     entry.isIntersecting
+//       ? entry.target.classList.add("active")
+//       : entry.target.classList.remove("active");
+//   });
+// });
 
-document
-  .querySelectorAll(".tab_content")
-  .forEach((tab_content) => io.observe(tab_content));
+// document
+//   .querySelectorAll(".tab_content")
+//   .forEach((tab_content) => io.observe(tab_content));
